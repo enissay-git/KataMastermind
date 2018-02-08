92e259c8e8a5044989c99c63b0253135ae524fec
enissay-git <yassine.aroua.dev@gmail.com>
2018-02-08 22:26:49 +0100

    modification test

--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -9,12 +9,23 @@ angular.module('mastermind.game', ['ngRoute'])
   });
 }])
 
-.controller('gameCtrl', [function() {
+.controller('gameCtrl', function($scope) {
 
   var vm = this;
+  $scope.secret = [];
+  $scope.guess = [];
+  $scope.trials = [];
+  vm.colorsChoice = [
+    {name: 'gray', code: '#808080'},
+    {name: 'red', code: '#FF0000'},
+    {name: 'yellow', code: '#FFFF00'},
+    {name: 'green', code: '#008000'},
+    {name: 'blue', code: '#0000FF'},
+    {name: 'purple', code: '#800080'},
+  ];
 
   vm.checkAnswer = (guess) => {
     return {wellPlaced: 1, missPlaced: 1};
   }
 
-}]);
+});
--- app/game/game_test.js
+++ app/game/game_test.js
@@ -22,34 +22,34 @@ describe('mastermind.game module', function() {
       expect(controller).toBeDefined();
     }));
 
-    it('checkAnswer with secret [blue, red, green, pink] should return {wellPlaced: 1, missPlaced: 1} if guess is  [yellow, red, blue, purple] ', inject(function($controller) {
-      $scope.secret = ['blue', 'red', 'green', 'pink'];
+    it('checkAnswer with secret [blue, red, green, purple] should return {wellPlaced: 1, missPlaced: 1} if guess is  [yellow, red, blue, grey] ', inject(function($controller) {
+      $scope.secret = ['blue', 'red', 'green', 'purple'];
       var response = {wellPlaced: 1, missPlaced: 1};
       $scope.guess = ['yellow', 'red', 'blue', 'purple'];
 
       expect(controller.checkAnswer($scope.guess)).toEqual(response);
     }));
 
-    it('checkAnswer with secret [blue] should return {wellPlaced: 0, missPlaced: 0} if guess is [red]', inject(function($controller) {
-      $scope.secret = ['red'];
+    it('checkAnswer with secret [blue, blue, blue, blue] should return {wellPlaced: 0, missPlaced: 0} if guess is [red, yellow, grey, grey]', inject(function($controller) {
+      $scope.secret = ['blue', 'blue', 'blue', 'blue'];
       var response = {wellPlaced: 0, missPlaced: 0};
-      $scope.guess = ['blue'];
+      $scope.guess = ['red', 'yellow', 'grey', 'grey'];
 
       expect(controller.checkAnswer($scope.guess)).toEqual(response);
     }));
 
-    it('checkAnswer with secret [blue] should return {wellPlaced: 1, missPlaced: 0} if guess is [blue]', inject(function($controller) {
-      $scope.secret = ['blue'];
+    it('checkAnswer with secret [blue, blue, blue, blue] should return {wellPlaced: 1, missPlaced: 0} if guess is [blue, grey, grey, grey]', inject(function($controller) {
+      $scope.secret = ['blue', 'blue', 'blue', 'blue'];
       var response = {wellPlaced: 1, missPlaced: 0};
-      $scope.guess = ['blue'];
+      $scope.guess = ['blue', 'grey', 'grey', 'grey'];
 
       expect(controller.checkAnswer($scope.guess)).toEqual(response);
     }));
 
-    it('checkAnswer with secret [red, yellow] should return {wellPlaced: 0, missPlaced: 1} if guess is [blue, red]', inject(function($controller) {
-      $scope.secret = ['red', 'yellow'];
+    it('checkAnswer with secret [red, yellow, grey, grey] should return {wellPlaced: 0, missPlaced: 1} if guess is [blue, red, green, green]', inject(function($controller) {
+      $scope.secret = ['red', 'yellow', 'grey', 'grey'];
       var response = {wellPlaced: 0, missPlaced: 1};
-      $scope.guess =  ['blue', 'red'];
+      $scope.guess =  ['blue', 'red', 'green', 'green'];
 
       expect(controller.checkAnswer($scope.guess)).toEqual(response);
     }));
