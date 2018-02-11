13fde3f0f7ee1510511ef4bc94f4d1e0880fedd9
enissay-git <yassine.aroua.dev@gmail.com>
2018-02-11 23:27:19 +0100

    Quick GUI for playing

--- app/game/game.controller.js
+++ app/game/game.controller.js
@@ -14,8 +14,10 @@ angular.module('mastermind.game', ['ngRoute'])
 
   var vm = this;
   vm.secret = [];
+  vm.secretSize = 4;
   vm.guess = [];
   vm.trials = [];
+  vm.cheat = false;
   vm.colorsChoice = [
     {name: 'gray', code: '#808080'},
     {name: 'red', code: '#FF0000'},
@@ -24,23 +26,61 @@ angular.module('mastermind.game', ['ngRoute'])
     {name: 'blue', code: '#0000FF'},
     {name: 'purple', code: '#800080'},
   ];
+  vm.currentEvaluation = {
+    missPlaced: 0,
+    wellPlaced: 0
+  };
+
+  function init() {
+    vm.generateSecret();
+  }
 
   vm.checkAnswer = () => {
     const evaluation = vm.checkWellPlaced(vm.guess, vm.secret);
-
+    vm.currentEvaluation = evaluation;
     return evaluation;
   }
 
   vm.setSecret = (secret) => {
-    if(secret.length === 4){
+    if(secret.length === vm.secretSize){
       vm.secret = secret;
     }
   }
 
+  vm.generateSecret = () => {
+    const secret = [];
+
+    for (let index = 0; index < vm.secretSize; index++) {
+      const indexColor = Math.floor(Math.random() * vm.colorsChoice.length);  
+      secret.push(vm.colorsChoice[indexColor].name);      
+    }
+
+    vm.setSecret(secret);
+  }
+
   vm.setGuess = (guess) => {
-    if(guess.length === 4) {
+    if(guess.length === vm.secretSize) {
       vm.guess = guess;
     }
+    vm.checkAnswer();
+  }
+
+  vm.getColorByName = (colorName) => {
+    let codeColor= "#000000";
+
+    if(vm.cheat) {
+      vm.colorsChoice.forEach((color, index) => {
+        if(color.name === colorName){
+          codeColor = color.code;
+        }
+      });
+    }
+    return codeColor;
+  }
+
+  vm.getStyle = function(name){
+    const color = vm.getColorByName(name);
+    return {"background-color": color};
   }
 
   vm.checkWellPlaced = (guess, secret) => {
@@ -52,7 +92,6 @@ angular.module('mastermind.game', ['ngRoute'])
     };
 
     secret.forEach((color, index) => {
-      $log.log(`Color secret : ${color} Color guess : ${guess[index]}, Index : ${index}`);
       if(guess[index] === color){
         wellPlacedResult.wellPlaced++;
       }
@@ -81,5 +120,7 @@ angular.module('mastermind.game', ['ngRoute'])
 
     return missPlacedResult;
   }
+
+  init();
 
 }]);
