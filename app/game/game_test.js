4a7040d1b6db43617e1879cfd5038cc44c1c856d
enissay <yassine.aroua.dev@gmail.com>
2018-02-14 11:48:25 +0100

    correction descriptif de test

--- app/game/game_test.js
+++ app/game/game_test.js
@@ -21,7 +21,7 @@ describe('mastermind.game module', function() {
       expect(controller).toBeDefined();
     }));
 
-    it('checkAnswer with secret [blue, red, green, purple] should return {wellPlaced: 1, missPlaced: 1} if guess is  [yellow, red, blue, grey] ', inject(function($controller) {
+    it('should indicate that one color is well placed and one is misplaced when user guess one good color at right place and one at the wrong', inject(function($controller) {
       var response = {wellPlaced: 1, missPlaced: 1};
       controller.setSecret(['blue', 'red', 'green', 'purple']);
       controller.setGuess(['yellow', 'red', 'blue', 'grey']);
@@ -29,7 +29,7 @@ describe('mastermind.game module', function() {
       expect(controller.checkAnswer()).toEqual(response);
     }));
 
-    it('checkAnswer with secret [blue, blue, blue, blue] should return {wellPlaced: 0, missPlaced: 0} if guess is [red, yellow, grey, grey]', inject(function($controller) {
+    it('should indicate that no color is well placed nor misplaced when user guessed totally wrong', inject(function($controller) {
       var response = {wellPlaced: 0, missPlaced: 0};
       controller.setSecret(['blue', 'blue', 'blue', 'blue']);
       controller.setGuess(['red', 'yellow', 'grey', 'grey']);
@@ -37,7 +37,7 @@ describe('mastermind.game module', function() {
       expect(controller.checkAnswer()).toEqual(response);
     }));
 
-    it('checkAnswer with secret [blue, blue, blue, blue] should return {wellPlaced: 1, missPlaced: 0} if guess is [blue, grey, grey, grey]', inject(function($controller) {
+    it('should indicate only one color is at the right place and not misplaced even if the user guess one color by put it multiple time', inject(function($controller) {
       var response = {wellPlaced: 1, missPlaced: 0};
       controller.setSecret(['blue', 'blue', 'blue', 'blue']);
       controller.setGuess(['blue', 'grey', 'grey', 'grey']);
@@ -45,7 +45,7 @@ describe('mastermind.game module', function() {
       expect(controller.checkAnswer()).toEqual(response);
     }));
 
-    it('checkAnswer with secret [red, yellow, grey, grey] should return {wellPlaced: 0, missPlaced: 1} if guess is [blue, red, green, green]', inject(function($controller) {
+    it('should indicate one color is misplaced when the user guessed one color but at the wrong location', inject(function($controller) {
       var response = {wellPlaced: 0, missPlaced: 1};
       controller.setSecret(['red', 'yellow', 'grey', 'grey']);
       controller.setGuess(['blue', 'red', 'green', 'green']);
